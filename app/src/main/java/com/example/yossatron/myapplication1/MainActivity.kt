7a723ccabae9839fcdfb45cbbe7afbe4267890d4
yossatron <yossatron@gmail.com>
2018-03-02 16:24:12 +0700

    desing btn

--- app/src/main/java/com/example/yossatron/myapplication1/MainActivity.kt
+++ app/src/main/java/com/example/yossatron/myapplication1/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.yossatron.myapplication1
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
+lateinit var btn1:Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn1 = findViewById(R.id.button)
+        btn1.setOnClickListener {
+            var xxx = Intent(this,Main2Activity::class.java)
+            startActivity(xxx)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+        }
     }
 }
